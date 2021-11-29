9a9f5e61d5e51448d3eece9de977791cfcd94ee0
Nicomedes77 <nicolas.vargas@bue.edu.ar>
2021-11-29 11:53:47 -0300

    mas ejercicios. Ejercicio final

---
+++ contador_ip/contador_ad/contador_ad.ip_user_files/bd/system/ipshared/f517/src/cont_user_logic.vhd
@@ -0,0 +1,45 @@
+-----------------------------------------------------------------
+-- Module Name: cont_user_logic
+-----------------------------------------------------------------
+
+library IEEE;
+use IEEE.std_logic_1164.all;
+use IEEE.numeric_std.all;
+
+entity cont_user_logic is
+	port(
+		clk_i: in std_logic;
+		char_i: in std_logic_vector(31 downto 0);
+		res_o: out std_logic_vector(31 downto 0)
+	);
+end;
+
+architecture cont_user_logic_arq of cont_user_logic is
+
+signal count : integer range 0 to 31:= 0;
+
+begin
+
+	process(clk_i)
+	begin
+      if rising_edge(clk_i) then           
+         if char_i = "00000000000000000000000000101011" then  -- signo (+)
+             count <= count + 1;
+         
+         elsif char_i = "00000000000000000000000000101101" then  -- signo (-)
+             count <= count - 1;
+             
+         --else
+             --count <= count + 2;           
+         end if;
+       end if;	   
+	
+--		if rising_edge(clk_i) then
+--			res_o <= std_logic_vector(unsigned(opA_i) + unsigned(opB_i));
+--		end if;
+	end process;
+	
+	res_o <= std_logic_vector(to_unsigned(count,32));
+
+end;
+
---
+++ laboratorio_4/laboratorio_4.sdk/TespApp/Debug/src/lab2.d
@@ -0,0 +1,43 @@
+src/lab2.o src/lab2.o: ../src/lab2.c \
+ ../../TespApp_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../TespApp_bsp/ps7_cortexa9_0/include/xparameters_ps.h \
+ ../../TespApp_bsp/ps7_cortexa9_0/include/xgpio.h \
+ ../../TespApp_bsp/ps7_cortexa9_0/include/xil_types.h \
+ ../../TespApp_bsp/ps7_cortexa9_0/include/xil_assert.h \
+ ../../TespApp_bsp/ps7_cortexa9_0/include/xstatus.h \
+ ../../TespApp_bsp/ps7_cortexa9_0/include/xgpio_l.h \
+ ../../TespApp_bsp/ps7_cortexa9_0/include/xil_io.h \
+ ../../TespApp_bsp/ps7_cortexa9_0/include/xil_printf.h \
+ ../../TespApp_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../TespApp_bsp/ps7_cortexa9_0/include/bspconfig.h \
+ ../../TespApp_bsp/ps7_cortexa9_0/include/xpseudo_asm.h \
+ ../../TespApp_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h \
+ ../../TespApp_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h
+
+../../TespApp_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../TespApp_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
+
+../../TespApp_bsp/ps7_cortexa9_0/include/xgpio.h:
+
+../../TespApp_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../../TespApp_bsp/ps7_cortexa9_0/include/xil_assert.h:
+
+../../TespApp_bsp/ps7_cortexa9_0/include/xstatus.h:
+
+../../TespApp_bsp/ps7_cortexa9_0/include/xgpio_l.h:
+
+../../TespApp_bsp/ps7_cortexa9_0/include/xil_io.h:
+
+../../TespApp_bsp/ps7_cortexa9_0/include/xil_printf.h:
+
+../../TespApp_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../TespApp_bsp/ps7_cortexa9_0/include/bspconfig.h:
+
+../../TespApp_bsp/ps7_cortexa9_0/include/xpseudo_asm.h:
+
+../../TespApp_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h:
+
+../../TespApp_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h:
